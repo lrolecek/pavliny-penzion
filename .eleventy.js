558fad36526ec8b719e7d4fc21c06ed2a8447aae
Luděk Roleček <ludek@rolecek.cz>
2021-04-30 08:48:27 +0200

    oprava

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,16 @@
-module.exports = function(eleventyConfig) {
-  // Výchozí výstupní složka je: _site
-  // Zkopírovat images/ do _site/images
-  eleventyConfig.addPassthroughCopy("images");
-  // Zkopírovat css/ to _site/css/
-  eleventyConfig.addPassthroughCopy("css");
+module.exports = function (eleventyConfig) {
+	// Výchozí výstupní složka je: _site
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy('images');
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy('css');
 
-  module.exports = function(eleventyConfig) {
-    // ...necháme, co už v konfiguraci je, a přidáme:
-    return {
-      // možné formáty šablon
-      templateFormats: ["njk","html", "md", "liquid"],
-      // jako šablonovací jazyk zvolíme Nunjucks`
-      markdownTemplateEngine: "njk",
-      htmlTemplateEngine: "njk",
-      dataTemplateEngine: "njk",
-    }
-  };
+	return {
+		// možné formáty šablon
+		templateFormats: ['njk', 'html', 'md', 'liquid'],
+		// jako šablonovací jazyk zvolíme Nunjucks`
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk',
+	};
 };
-
